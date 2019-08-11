9ed68c1c50f315dbfe3d3557083e017df0c4667a
T.Shinohara <takuma1201@gmail.com>
2019-08-11 15:04:09 +0900

    initial commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+var tipData = new Vue({
+    el: '#formdata',
+    computed: {
+        text: function() {
+            return this.setText;
+        },
+    },
+    data: {
+        setText: "",
+        notset: true
+    },
+    methods: {
+        clearNotImage: function() {
+            this.notset = false;
+        }
+    }
+});
+
+$('#setText').on('click', function() {
+    console.log(tipData.text);
+    $("#generatedQR").html("");
+    $("#generatedQR").qrcode({ width: 240, height: 240, text: tipData.text });
+});
